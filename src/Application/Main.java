ddb3a3765cb8589bef328965e1ef058d70f54451
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-04 03:58:05 -0300

    mini projeto feito v 1.0.0

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,2 +1,16 @@
-package Application;public class Main {
+package Application;
+
+import Entities.Conta;
+import Entities.Login;
+import Interface.Interface;
+import Interface.Ui;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Interface sistema = new Interface();
+        sistema.exibirInterface();
+
+    }
 }
