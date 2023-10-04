830bbd7665f4b6b70977ace20a703de75dee6c93
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-04 01:51:49 -0300

    criada estrutura para manipular as principais funcionalidades da aplicacao

--- src/Interface/Interface.java
+++ src/Interface/Interface.java
@@ -1,2 +1,49 @@
-package Interface;public class Interface {
+package Interface;
+
+import Entities.Conta;
+import Entities.Login;
+
+import java.util.ArrayList;
+
+public class Interface {
+    static ArrayList<Conta> contas = new ArrayList<>();
+    static Ui interacao = new Ui();
+    static Login login = new Login();
+
+    public void exibirInterface() {
+        int opcao = 0;
+        int opcaoLogin = 0;
+
+        do {
+            opcao = interacao.menuLogin();
+
+            if (opcao == 1) {
+                login.criarConta(contas);
+
+            } else if (opcao == 2) {
+                boolean vF = login.fazerLogin(contas);
+
+                if (vF) {
+                    do {
+                        opcaoLogin = interacao.menuContas();
+
+                        if (opcaoLogin == 1) {
+
+                        } else if (opcaoLogin == 2) {
+
+
+                        } else if (opcaoLogin == 3) {
+
+
+                        } else if (opcaoLogin == 4) {
+
+
+                        }
+                    } while (opcaoLogin != 0);
+                }
+            }
+        } while (opcao != 0);
+
+
+    }
 }
